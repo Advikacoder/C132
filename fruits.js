7d177bfdf5d8536d90cfc8a202e36951d78ec6b2
Advikacoder <92722900+Advikacoder@users.noreply.github.com>
2022-06-18 10:04:56 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,45 @@
+
+img = "";
+status = "";
+
+
+function preload()
+{
+    img = loadImage('fruitjpg.jpg');
+}
+
+function setup() 
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    satus = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function back()
+{
+    window.location = "index.html"
+}
+  
---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+function animals()
+{
+    window.location = "animals.html";
+}
+
+function fruits()
+{
+    window.location = "animals.html";
+}
+
+function bedroom()
+{
+    window.location = "animals.html";
+}
+
+function livingroom()
+{
+    window.location = "animals.html";
+}
